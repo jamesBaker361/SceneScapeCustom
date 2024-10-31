50d50d6451ed76bc7b3b5e9c733335c6fcbc8e45
James Baker <jlbaker361@gmail.com>
2024-10-31 10:32:31 -0400

    aws isntall

---
+++ install_aws.sh
@@ -0,0 +1,12 @@
+pip cache purge
+conda clean --all -y
+conda create --name scenescape python=3.10 -y
+conda activate scenescape 
+conda install pytorch=1.13.0 torchvision pytorch-cuda=11.6 -c pytorch -c nvidia -y
+conda install -c fvcore -c iopath -c conda-forge fvcore iopath -y
+conda install -c bottler nvidiacub -y
+conda install pytorch3d -c pytorch3d -y
+pip install git+https://github.com/omry/omegaconf.git
+pip install -r requirements.txt
+pip install experiment-helpers
+python run.py
